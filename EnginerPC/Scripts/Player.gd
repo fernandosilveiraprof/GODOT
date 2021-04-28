7d2f7b55e664a3b4887cd3a3cb6f31cc18e1c071
Luckym4n3103 <Lukinha3103.lm@gmail.com>
2021-04-28 15:59:54 -0300

    atualizacao das cameras

--- EnginerPC/Scripts/Player.gd
+++ EnginerPC/Scripts/Player.gd
@@ -16,6 +16,14 @@ var mouse_sensivity = 0.1
 
 onready var head = $Head
 onready var camera = $Head/Camera
+onready var reach = $Head/Camera/RayCast
+onready var hand = $Head/hand
+
+onready var item_a_hr = preload("res://Scenes/Item1HD.tscn")
+onready var item_a = preload("res://Scenes/Item1.tscn")
+
+var item_to_spawn
+var item_to_drop
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
